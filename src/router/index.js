e704f06dd251880fcdfa8426bdc4129c9710faa7
Zack <zackbrave@outlook.com>
2022-03-26 06:28:20 +0800

    fix: title

--- src/router/index.js
+++ src/router/index.js
@@ -5,29 +5,44 @@ const routes = [
   {
     path: '/',
     redirect: {
-      name: 'service-list'
-    }
+      name: 'service-list',
+    },
   },
   {
     path: '/service-list',
     name: 'service-list',
-    component: ServiceListView
+    component: ServiceListView,
+    meta: {
+      title: 'System Service List',
+    },
   },
   {
     path: '/configs',
     name: 'configs',
-    component: () => import('../views/ConfigsView.vue')
+    component: () => import('../views/ConfigsView.vue'),
+    meta: {
+      title: 'Service Configs',
+    },
   },
   {
     path: '/metrics',
     name: 'metrics',
-    component: () => import('../views/MetricsView.vue')
-  }
+    component: () => import('../views/MetricsView.vue'),
+    meta: {
+      title: 'Service Metrics',
+    },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+})
+
+const defaultTitle = 'System Service List'
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title ? to.meta.title : defaultTitle
+  next()
 })
 
 export default router
